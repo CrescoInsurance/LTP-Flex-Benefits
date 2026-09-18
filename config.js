1d9baedd6efe35733edf32b219c2a3e5a2769de7
Insured <tintecksiong@gmail.com>
2026-09-18 18:51:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase project connection settings for the Flex Benefits Portal.
+window.SUPABASE_CONFIG = {
+  url: 'https://txiklhwsahotrksanyzd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4aWtsaHdzYWhvdHJrc2FueXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjU3MzIsImV4cCI6MjEwNTMwMTczMn0.rupttdTYhLTtiCzlx3KX9VHaeNxM9f9wUR2LxDRthiE'
+};
